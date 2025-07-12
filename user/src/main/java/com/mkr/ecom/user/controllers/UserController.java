74eaf26e243ceebb056eca162d495a1318009cc6
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-07-12 10:51:18 +0530

    configuration management in ecommerce microservices

--- user/src/main/java/com/mkr/ecom/user/controllers/UserController.java
+++ user/src/main/java/com/mkr/ecom/user/controllers/UserController.java
@@ -4,6 +4,8 @@ import com.mkr.ecom.user.dto.UserRequest;
 import com.mkr.ecom.user.dto.UserResponse;
 import com.mkr.ecom.user.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class UserController {
 
+    public static final Logger log = LoggerFactory.getLogger(UserController.class);
     private final UserService userService;
 
     @GetMapping("/users")
@@ -24,6 +27,8 @@ public class UserController {
 
     @GetMapping("/users/{id}")
     public ResponseEntity<UserResponse> getUsers(@PathVariable String id) {
+        log.info("Request received for user: {}", id);
+        log.trace("Request details");
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
@@ -31,7 +36,6 @@ public class UserController {
 
     @PostMapping("/users")
     public ResponseEntity<String> createUser(@RequestBody UserRequest user) {
-
         userService.addUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body("User created successfully");
     }
