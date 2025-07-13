5b5bd98ea4946386f134f3f90c4cad3345992c3c
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-07-13 21:06:36 +0530

    role-based authorization using DB using password encoder

--- additional/spring-sec/src/main/java/com/mkr/secure/springsec/config/SecurityConfig.java
+++ additional/spring-sec/src/main/java/com/mkr/secure/springsec/config/SecurityConfig.java
@@ -1,27 +1,39 @@
 package com.mkr.secure.springsec.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Autowired
+    DataSource dataSource;
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((req) ->
                         req
+                                .requestMatchers("/h2-console/**").permitAll()
                                 .requestMatchers("/contact").permitAll()
 //                                .requestMatchers("/admin").denyAll()
 //                                .requestMatchers("/admin/**").denyAll()
@@ -29,6 +41,10 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 //                .formLogin(Customizer.withDefaults()) // comment to diable the form login
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+//                .csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**")) // Disable CSRF for H2 console
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for H2 console
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
@@ -36,13 +52,31 @@ public class SecurityConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         var user = User.withUsername("user1")
-                .password("{noop}user1")
+                .password(passwordEncoder().encode("user1"))
+//                .password("{noop}user1")
                 .roles("USER")
                 .build();
         var admin = User.withUsername("admin")
-                .password("{noop}admin")
+                .password(passwordEncoder().encode("admin"))
+//                .password("{noop}admin")
                 .roles("ADMIN")
                 .build();
-        return new InMemoryUserDetailsManager(user, admin);
+
+        // Use JdbcUserDetailsManager to store users in the database
+        // Ensure that the DataSource is properly configured in your application
+        // and that the necessary tables (like users, authorities) exist.
+        // Use schema.sql to create the necessary tables
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(admin);
+        return userDetailsManager;
+
+        // Use InMemoryUserDetailsManager to store users in memory
+//        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
