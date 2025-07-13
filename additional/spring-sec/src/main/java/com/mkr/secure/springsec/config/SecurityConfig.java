77f8f610266ba399992f6c107edc1c38788890cc
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-07-13 12:04:25 +0530

    add custom security config

---
+++ additional/spring-sec/src/main/java/com/mkr/secure/springsec/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.mkr.secure.springsec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((req) -> req.anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults()) // comment to diable the form login
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
