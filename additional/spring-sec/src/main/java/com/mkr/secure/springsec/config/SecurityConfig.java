b4198f230523431e824863fd2dd1b25d7ea367e8
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-07-13 12:08:13 +0530

    add statelessness in custom security config

--- additional/spring-sec/src/main/java/com/mkr/secure/springsec/config/SecurityConfig.java
+++ additional/spring-sec/src/main/java/com/mkr/secure/springsec/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,6 +16,8 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((req) -> req.anyRequest().authenticated())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 //                .formLogin(Customizer.withDefaults()) // comment to diable the form login
                 .httpBasic(Customizer.withDefaults());
         return http.build();
