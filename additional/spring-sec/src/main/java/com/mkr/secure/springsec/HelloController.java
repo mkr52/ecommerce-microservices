2181d3f5646cf97b32b022fdf79dc1655f1d69a7
mkr52 <161114463+mkr52@users.noreply.github.com>
2025-07-13 11:07:52 +0530

    spring security practice initial commit

---
+++ additional/spring-sec/src/main/java/com/mkr/secure/springsec/HelloController.java
@@ -0,0 +1,13 @@
+package com.mkr.secure.springsec;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greeting() {
+        return "Hello World!";
+    }
+}
